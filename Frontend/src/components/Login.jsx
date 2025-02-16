56270763f1b8e27633f296fa8d7cdf567fd4f780
Akshat Pratyush <akshatp0505@gmail.com>
2025-02-17 01:18:43 +0530

    imported axios

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     return (
